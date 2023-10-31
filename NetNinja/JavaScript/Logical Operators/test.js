3a8e26b86bb3ef4fd08da159181a40c6ead52581
losalmson <mialo.salmson@gmail.com>
2023-10-31 20:47:00 +0100

    My first if statements with logical operators

---
+++ NetNinja/JavaScript/Logical Operators/test.js
@@ -0,0 +1,21 @@
+// The && and operator
+var myAge = 35;
+
+if (myAge >= 18 && myAge <= 30) {
+
+    document.write("You can come, you cool dude");
+} else {
+    
+    document.write("You ain't coming!");
+}
+
+// The || or operator 
+var myShoppingList = ("bread");
+
+if (myShoppingList === "apples" || myShoppingList === "oranges") {
+
+    document.write(" ", "Yay you found some fruit!");
+} else {
+
+    document.write(" ", "Please find some fruit :(");
+}
