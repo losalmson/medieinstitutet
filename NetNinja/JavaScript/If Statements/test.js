455b9065047474aa53e03a6fb27b07675e005b8f
losalmson <mialo.salmson@gmail.com>
2023-10-31 20:27:43 +0100

    My first if ... and else statement in JavaScript

---
+++ NetNinja/JavaScript/If Statements/test.js
@@ -0,0 +1,22 @@
+// My first if statement
+var myNum = 8;
+
+if (myNum === 10) {
+    
+    document.write("myNum is equal to 10");
+
+} else {
+
+    document.write("myNum is not equal to 10.", " ")
+}
+
+// My second if statement 
+var youLikeMeat = false; 
+
+if (youLikeMeat) {
+
+    document.write("You like meat!");
+} else {
+
+    document.write("You dont like meat :(")
+}
